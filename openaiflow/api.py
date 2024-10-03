d5b7966fc1fce9cd14ed2adc99d72c7dd387d44b
ikotun-dev <danlogan2003@gmail.com>
2024-10-03 23:28:49 +0100

    testing wrong api keys

--- openaiflow/api.py
+++ openaiflow/api.py
@@ -6,6 +6,18 @@ import openai
 # import time
 dotenv.load_dotenv()
 
+# TODO: Add a method to check the validity of the api
+# TODO: create assistant
+# TODO: create a thread
+# TODO: store messages ( in memory ) for a thread
+
+
+"""
+MESSAGES_URL = (
+    f"https://api.openai.com/v1/threads/{thread_id}/messages?limit=1&order=desc"
+)
+"""
+
 
 class OpenaiWrapper:
     def __init__(self, api_key):
@@ -13,10 +25,18 @@ class OpenaiWrapper:
         openai.api_key = self._api_key
         self.client = None
 
+        self.headers = {
+            "Authorization": f"Bearer {self._api_key}",
+            "Content-Type": "application/json",
+            "OpenAI-Beta": "assistants=v2",
+        }
+
     def validate_api_key(self):
+        """
+        tests validity of the api_key by creating a message
+        """
         try:
             client = openai.OpenAI()
-            # testing validity of the api_key by creating a message
             _ = client.chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
@@ -25,11 +45,16 @@ class OpenaiWrapper:
                 ],
             )
 
+            print(_.choices[0])
             print("API key is valid")
         except Exception as e:
             print(f"Error : {e}")
             print("API key is invalid")
 
+    def create_assistant(self):
+        pass
 
-client = OpenaiWrapper(os.getenv("KEY"))
+
+# client = OpenaiWrapper(os.getenv("KEY"))
+client = OpenaiWrapper("ah")
 client.validate_api_key()
